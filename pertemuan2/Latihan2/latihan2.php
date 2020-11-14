da6136d05ee9e4560198ef8692b4835c1c52f7fa
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-14 22:30:47 +0700

    Update latihan2.php

--- pertemuan2/Latihan2/latihan2.php
+++ pertemuan2/Latihan2/latihan2.php
@@ -3,7 +3,13 @@
 $data = array("lanirne", "aduh", "qifuad", "toda", "anevi", "samid", "kifuat");
 
 echo "Data : ";
+echo "</br>";
 var_dump($data);
 echo "</br>";
+echo "</br>";
 
+echo "Data diurutkan : ";
+echo "</br>";
+sort($data);
+var_dump($data)
  ?>
