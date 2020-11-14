ccfc7c704bc20e20cf20b02a4154d4ed239923e6
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-14 22:07:03 +0700

    Update latihan1.php

--- pertemuan2/Latihan1/latihan1.php
+++ pertemuan2/Latihan1/latihan1.php
@@ -7,9 +7,12 @@ echo "bilangan 1 = " . strval($bil1);
 echo "</br>";
 echo "bilangan 2 = " . strval($bil2);
 echo "</br>";
+echo "</br>";
+
 echo "Berikut merupakan hasil dari setiap operasi";
 echo "</br>";
 echo "</br>";
+
 echo "PENJUMLAHAN";
 echo "</br>";
 echo "Operator : + ";
@@ -17,6 +20,7 @@ echo "</br>";
 echo "Hasil : " . strval($bil1 + $bil2);
 echo "</br>";
 echo "</br>";
+
 echo "PENGURANGAN";
 echo "</br>";
 echo "Operator : - ";
@@ -24,12 +28,14 @@ echo "</br>";
 echo "Hasil : " . strval($bil1 - $bil2);
 echo "</br>";
 echo "</br>";
+
 echo "PERKALIAN";
 echo "</br>";
 echo "Operator : * ";
 echo "</br>";
 echo "Hasil : " . strval($bil1 * $bil2);
 echo "</br>";
+
 echo "PEMBAGIAN";
 echo "</br>";
 echo "Operator : / ";
@@ -37,6 +43,7 @@ echo "</br>";
 echo "Hasil : " . strval($bil1 / $bil2);
 echo "</br>";
 echo "</br>";
+
 echo "MODULUS";
 echo "</br>";
 echo "Operator : % ";
