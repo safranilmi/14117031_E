08cec6a0dff214993264564ca50cc0010dc1444f
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 23:10:57 +0700

    Update view.php

--- pertemuan3/Modul 7 MySQL/Latihan/view.php
+++ pertemuan3/Modul 7 MySQL/Latihan/view.php
@@ -1,2 +1,9 @@
 <?php 
+	$conn = mysqli_connect("localhost","root","");
+	mysqli_select_db($conn, "tamu");
+	$hasil = mysqli_query($conn, "select * from bukutamu");
+	$jumlah = mysqli_num_rows($hasil);
+	echo "<center>Daftar Pengunjung</center>";
+	echo "Jumlah pengunjung : $jumlah";
+	$a = 1;
  ?>
