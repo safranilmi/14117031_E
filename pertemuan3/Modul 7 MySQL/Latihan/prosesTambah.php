629071883e6618f5d7f20962f5e352695552f4aa
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 23:08:56 +0700

    Update prosesTambah.php

--- pertemuan3/Modul 7 MySQL/Latihan/prosesTambah.php
+++ pertemuan3/Modul 7 MySQL/Latihan/prosesTambah.php
@@ -6,6 +6,19 @@
 <body>
 
 	<h1>Simpan Buku Tamu MySQLi</h1>
+	<?php 
+		$nama = $_POST["nama"];
+        $email = $_POST["email"];
+        $komentar = $_POST["komentar"];
+        $conn = mysqli_connect("localhost", "root", "") or die("koneksi gagal");
+        mysqli_select_db($conn, "mahasiswa");
+        echo "Nama : $nama </br>";
+        echo "Email : $email <br>";
+        echo "Komentar : $komentar <br>";
+        $sqlstr = "INSERT INTO bukutamu (nama, email, komentar) values ('$nama','$email','$komentar')";
+        $hasil = mysqli_query($conn, $sqlstr);
+        echo "Simpan bukutamu berhasil dilakukan";		
+	 ?>
 
 </body>
 </html>
