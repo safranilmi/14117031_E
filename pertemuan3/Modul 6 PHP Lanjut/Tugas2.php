9b9280674a213fa3753f0e73280d1e9ba0e873a1
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 21:29:06 +0700

    Update Tugas2.php

--- pertemuan3/Modul 6 PHP Lanjut/Tugas2.php
+++ pertemuan3/Modul 6 PHP Lanjut/Tugas2.php
@@ -1,2 +1,18 @@
 <?php 
+
+function hargaBet($nama, $warna="red"){
+    $harga = 0;
+    $panjang = strlen($nama);
+
+    if($panjang > 20){
+        $harga = $panjang * 700;
+    }elseif ($panjang > 10) {
+        $harga = $panjang * 500;
+    }elseif ($panjang > 0) {
+        $harga = $panjang * 300;
+    }
+
+    echo '<p style="color: '.$warna.'">Bet '.$nama.' dengan harga '.strval($harga).'</p>';
+}
+
  ?>
