415ea02a49051d0b493c67c17cabc2af410cd790
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 23:23:44 +0700

    Update hasilsearch.php

--- pertemuan3/Modul 7 MySQL/Latihan/hasilsearch.php
+++ pertemuan3/Modul 7 MySQL/Latihan/hasilsearch.php
@@ -8,4 +8,10 @@
 	echo "<br>";
 	echo "Ditemukan : $jumlah";
 	echo "<br>";
+	while($baris = mysqli_fetch_array($hasil)){
+    	echo "Nama : "; echo $baris[0]; echo "<br>";
+    	echo "Email : "; echo $baris[1]; echo "<br>";
+    	echo "Komentar :";
+    	echo $baris[2];
+}
  ?>
