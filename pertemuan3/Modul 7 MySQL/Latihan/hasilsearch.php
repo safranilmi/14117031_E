7e5d5ce09136905ec3727f9e616b9dad6300c615
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 23:21:52 +0700

    Update hasilsearch.php

--- pertemuan3/Modul 7 MySQL/Latihan/hasilsearch.php
+++ pertemuan3/Modul 7 MySQL/Latihan/hasilsearch.php
@@ -1,2 +1,11 @@
 <?php 
+	$kolom = $_POST['kolom'];
+	$cari = $_POST['cari'];
+	$conn = mysqli_connect("localhost", "root", "");
+	mysqli_select_db($conn, "tamu");
+	$hasil = mysqli_query($conn, "SELECT * FROM bukutamu WHERE $kolom like '%$cari%'");
+	$jumlah = mysqli_num_rows($hasil);
+	echo "<br>";
+	echo "Ditemukan : $jumlah";
+	echo "<br>";
  ?>
