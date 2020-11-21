c2d1a351f7d404df6507a4a6635a93a0df4f8ca1
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 23:12:55 +0700

    Update view.php

--- pertemuan3/Modul 7 MySQL/Latihan/view.php
+++ pertemuan3/Modul 7 MySQL/Latihan/view.php
@@ -6,4 +6,15 @@
 	echo "<center>Daftar Pengunjung</center>";
 	echo "Jumlah pengunjung : $jumlah";
 	$a = 1;
+	while($baris = $mysqli_fetch_array($hasil)){
+    	echo "<br>";
+    	echo $a;
+    	echo "<br>"; echo "Nama : "; echo $baris[0]; echo "<br>";
+    	echo "Email : ";
+    	echo $baris[1];
+    	echo "<br>";
+    	echo "Komentar : ";
+    	echo $baris[2];
+    $a++;
+}
  ?>
