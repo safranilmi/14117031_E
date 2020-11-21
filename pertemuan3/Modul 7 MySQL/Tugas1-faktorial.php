98717df91e4f609ec7a0324bdb6bd000849fe92b
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 21:21:31 +0700

    Create Tugas1-faktorial.php

---
+++ pertemuan3/Modul 7 MySQL/Tugas1-faktorial.php
@@ -0,0 +1,13 @@
+<?php 
+
+function faktorial($x){
+	$angka = 1;
+	$faktorial = 1;
+	while($angka <= $x){
+		$faktorial = $faktorial * $angka;
+		$angka = $angka + 1;
+	}
+	return $faktorial;
+}
+echo "$faktorial";
+ ?>
