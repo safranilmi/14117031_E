82fe36741a7ce6606343fc771325532eb82a9de5
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-11-21 22:04:46 +0700

    Update index.php

--- pertemuan3/Modul 7 MySQL/index.php
+++ pertemuan3/Modul 7 MySQL/index.php
@@ -8,6 +8,19 @@
     <h2>===================</h2>
     <h2>TAMBAH DATA</h2>
 
+    <form method="POST" enctype="multipart/form-data">
+        NRP : <input type="text" name="nrp"> <br>
+        Nama : <input type="text" name="nama"> <br>
+        Foto : <input type="text" name=""><input type="file" name="foto" > <br>
+        Jurusan :   <select name="jurusan">
+                        <option value="Sains">Telekomunikasi</option>
+                        <option value="Teknik">Elka</option>
+                        <option value="Teknik">IT</option>
+                        <option value="Teknik">Elin</option>
+                    </select> <br>
+        <input type="submit" name="submit" value="Tambah">
+    </form>
+
     <h2>===================</h2>
     <h2>SEARCH DATA</h2>
     <form action="" method="POST">
