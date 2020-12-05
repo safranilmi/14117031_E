76a81f9a88175f49b3b21b51fdf03b82be54e521
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-12-05 21:18:47 +0700

    Update login.php

--- pertemuan4/Session & Cookies/login.php
+++ pertemuan4/Session & Cookies/login.php
@@ -4,4 +4,22 @@ include_once('user.php');
 
 $user = new user();
 
+if(isset($_POST['login'])){
+    $username = $user->escape_string($_POST['username']);
+    $password = $user->escape_string($_POST['password']);
+
+    $auth = $user->check_login($username, $password);
+
+    if(!$auth){
+        $_SESSION['message'] = 'User tidak terdaftar';
+        header('location:index.php');
+    }else{
+        $_SESSION['user'] = $auth;
+        header('location:home.php');
+    }
+}else{
+    $_SESSION['message'] = 'Silahkan login';
+    header('location:index.php');
+}
+
  ?>
